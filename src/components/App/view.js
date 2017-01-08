a338ae89eb278c85d64b1e51a42f21c5ac324b55
kimdhoe <kimdhoe1@gmail.com>
2017-01-08 10:33:10 +0900

    Use the latest josa.

--- src/components/App/view.js
+++ src/components/App/view.js
@@ -1,14 +1,54 @@
-import { div, h1, input, p, span } from '@cycle/dom'
+import { a, div, em, h1, input, p, span } from '@cycle/dom'
 
 const listOfJosa =
   [ '은', '는', '이', '가', '을', '를', '와', '과', '이어', '여', '으로', '로', '이에요', '예요', '이었', '였', '아', '야', '이?' ]
 
 const renderHeader = () =>
   div( '.Header'
-     , [ h1('.Header-title', ['Josa Test REPL'])
+     , [ h1('.Header-title', [ 'JOSA TEST REPL' ])
+       , p( '.Header-meta'
+          , [ a( '.Header-author'
+               , { props: { href: 'https://github.com/kimdhoe' } }
+               , [ '©Kimdhoe' ]
+               )
+            , '. '
+            , a( '.Header-link'
+               , { props: { href: 'https://github.com/kimdhoe/josa' } }
+               , [ '(GitHub)' ]
+               )
+            , a( '.Header-link'
+               , { props: { href: 'https://www.npmjs.com/package/josa' } }
+               , [ '(npm)' ]
+               )
+            ]
+          )
        , p( '.Header-description'
+       , [ a( { props: { href: 'https://github.com/kimdhoe/josa' } }
+               , 'Josa'
+               )
+            , '는 동적으로 생성된 문자열에 적법한 조사를 선택/삽입하기 위해 만들어진 한글 관련 JavaScript 라이브러리입니다.'
+            ]
+          )
+       , p( '.Header-description'
+          , [ '이 REPL은 현재 '
+            , a( { props: { href: 'https://www.npmjs.com/package/josa' } }
+               , 'Josa@v2.0.1'
+               )
+            , '을 사용하고 있으며, 지원하는 플레이스홀더는 다음과 같습니다:'
+            ]
+          )
+       , p( '.Header-josaList'
           , listOfJosa.map(josa => span('.Header-josa', [ `#{${josa}}` ]))
           )
+       , p( '.Header-exampleTitle'
+          , [ '예시:' ]
+          )
+       , p( '.Header-example'
+          , [ '> 친구#{이} 선생님#{와} 함께 학교#{으로} 갑니다.' ]
+          )
+       , em( '.Header-example'
+           , [ '친구가 선생님과 함께 학교로 갑니다.' ]
+           )
        ]
      )
 
